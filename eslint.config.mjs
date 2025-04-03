93524522ea499214233f943072db78020968289d
florian-lup <contact@florianup.com>
2025-04-03 13:53:32 +0100

    Add Prettier and ESLint configuration files, update package dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier"
+  ),
+  {
+    rules: {
+      "prettier/prettier": "error",
+    },
+    plugins: ["prettier"],
+  },
 ];
 
 export default eslintConfig;
